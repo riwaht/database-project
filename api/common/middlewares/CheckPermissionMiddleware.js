6a602d319f7a0d0fc33c2bb746f9fb5dc36f1305
Charbel <charbelelbateh@gmail.com>
2023-12-11 00:34:21 +0200

    Added login endpoint

--- api/common/middlewares/CheckPermissionMiddleware.js
+++ api/common/middlewares/CheckPermissionMiddleware.js
@@ -1,30 +1,47 @@
+const jwt = require("jsonwebtoken");
 const UserModel = require("../models/User");
 
 module.exports = {
   has: (role) => {
     return (req, res, next) => {
-      const {
-        user: { userId },
-      } = req;
+      const token = req.headers.authorization && req.headers.authorization.split(" ")[1];
 
-      UserModel.findUser({ id: userId }).then((user) => {
-        if (!user) {
+      if (!token) {
+        return res.status(403).json({
+          status: false,
+          error: "Invalid access token provided, please login again.",
+        });
+      }
+
+      jwt.verify(token, process.env["JWT_SECRET"], (err, decoded) => {
+        if (err) {
           return res.status(403).json({
             status: false,
-            error: "Invalid access token provided, please login again.",
+            error: "Failed to authenticate token.",
           });
         }
 
-        const userRole = user.role;
+        const { userId } = decoded; // Extract user ID from the decoded token
 
-        if (userRole !== role) {
-          return res.status(403).json({
-            status: false,
-            error: `You need to be a ${role} to access this endpoint.`,
-          });
-        }
+        UserModel.findUser({ id: userId }).then((user) => {
+          if (!user) {
+            return res.status(403).json({
+              status: false,
+              error: "Invalid access token provided, please login again.",
+            });
+          }
 
-        next();
+          const userRole = user.role;
+
+          if (userRole !== role) {
+            return res.status(403).json({
+              status: false,
+              error: `You need to be a ${role} to access this endpoint.`,
+            });
+          }
+
+          next();
+        });
       });
     };
   },
